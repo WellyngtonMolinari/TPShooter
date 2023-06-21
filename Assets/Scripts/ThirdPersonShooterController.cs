7e93d7eed800e287f11ba5ed1463614182fba02e
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-21 11:00:07 -0300

    Aim Camera Update
    Now the player face the direction of aim when camera zooms in

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -24,10 +24,26 @@ public class ThirdPersonShooterController : MonoBehaviour
     }
     private void Update()
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+
         if (starterAssetsInputs.aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
             thirdPersonController.SetSensitivity(aimSensitivity);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+
+            transform.forward = Vector3.Lerp(transform.forward,aimDirection, Time.deltaTime * 20f);
         }
         else
         {
@@ -35,11 +51,5 @@ public class ThirdPersonShooterController : MonoBehaviour
             thirdPersonController.SetSensitivity(normalSensitivity);
         }
 
-        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
-        {
-            debugTransform.position = raycastHit.point;
-        }
     }
 }
