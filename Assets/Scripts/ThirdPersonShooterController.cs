d25cf2cf57cf3cddc77e866b496898790e7b612d
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-21 08:29:06 -0300

    New project start

---
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera aimVirtualCamera;
+
+    private void Update()
+    {
+        
+    }
+}
