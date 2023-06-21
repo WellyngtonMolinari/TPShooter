70267bd83d3167262ddd8165ddb941f27a810d52
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-21 10:01:57 -0300

    Added new sensitivity when zoom in

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -7,23 +7,29 @@ using StarterAssets;
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+
 
     private StarterAssetsInputs starterAssetsInputs;
+    private ThirdPersonController thirdPersonController;
 
     private void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
-
+        thirdPersonController = GetComponent<ThirdPersonController>();
     }
     private void Update()
     {
         if (starterAssetsInputs.aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivity(aimSensitivity);
         }
         else
         {
             aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivity(normalSensitivity);
         }
     }
 }
